94d695e4441371c75580dabbb6d63ce0a3182a7f
Justin Daniel <justinddaniel@gmail.com>
2017-11-01 22:47:48 +0000

    Defined routes that happen after signup.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,31 @@ class ApplicationController < Sinatra::Base
     erb :'/users/signup'
   end
 
+  post '/signup' do
+    user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if user.save
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    else
+      redirect '/signup'
+    end
+  end
+
   get '/login' do
     erb :'users/login'
   end
 
   get '/user-agreement' do
     erb :'users/agreement'
+  end
+
+  get '/users/:id' do
+    if logged_in?
+      @user = User.find_by_slug(params[:id])
+      erb :'users/items'
+    else
+      redirect '/login'
+    end
   end
 
 end
