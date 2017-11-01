e0306b78e4a5501231f3551a48d35a1f7bbe9339
Justin Daniel <justinddaniel@gmail.com>
2017-11-01 22:53:49 +0000

    Added helper methods. Confirmed signup route to post and id works.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,4 +43,17 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+
+
+  helpers do
+   def logged_in?
+     !!session[:user_id]
+   end
+
+   def current_user
+     User.find(session[:user_id])
+   end
+
+ end
+
 end
