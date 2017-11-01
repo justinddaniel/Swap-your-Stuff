e64f20f8456da9f42155100d13cd1b831db068cd
Justin Daniel <justinddaniel@gmail.com>
2017-11-01 23:08:08 +0000

    set up login routes and redirects for it.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
-    erb :'users/login'
+    if logged_in?
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    else
+      redirect "/login"
+    end
   end
 
   get '/user-agreement' do
